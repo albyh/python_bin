81f04efdebff21fc7995048f0dbafdbe66cf1a52
albyh <albyheredia@gmail.com>
2016-07-11 12:32:24 -0700

    OO MVP implemented

--- item59_1.py
+++ item59_1.py
@@ -1,4 +1,4 @@
-import Tkinter as tk, Tkconstants, tkFileDialog
+import Tkinter as tk, Tkconstants, tkFileDialog, tkMessageBox
 import shutil
 import os
 import glob
@@ -6,14 +6,12 @@ import datetime
 import time
 
 def setup():
-
     win = tk.Tk()        
     win.title("Send Files to HQ")
     srcPath  = ""
     destPath = ""
     paths    = {"src"   : "",
                 "dest"  : ""}
-
     text = [
         "Send Files to HQ", 
         "This application will move files modified or edited in the past 24 hours",
@@ -27,6 +25,10 @@ def setup():
     options['parent'] = win
     options['title'] = ''
 
+    results = { 'moved'  : [],
+                'skipped': []
+              }
+
     tk.Label(win, text = text[0], font = ('Arial', 18, 'bold'), pady=20).pack()
     tk.Frame(win, height = 10).pack()
     tk.Label(win, text = text[1]).pack()
@@ -38,7 +40,7 @@ def setup():
     con1 = tk.Frame(win, height=100, width = 200, padx=10, pady=10, bd=2, relief='groove' )
     con1.pack()
 
-    # options for buttons
+    # options for src/dest buttons
     button_opt = { 'fill': Tkconstants.BOTH, 'padx': 80, 'pady': 10}
 
     # define buttons
@@ -57,7 +59,7 @@ def setup():
 
     tk.Frame(win, height = 30).pack()
 
-    bCopy   = tk.Button(win, state='disabled', text='Move Staged Files', pady = 10, command= lambda: moveFiles(paths))
+    bCopy   = tk.Button(win, state='disabled', text='Move Staged Files', pady = 10, command= lambda: moveFiles(paths, results))
     bCopy.pack() #**button_opt)
 
     tk.Frame(win, height = 50).pack()
@@ -65,18 +67,16 @@ def setup():
     return win
 
 def okToCopy(paths):
-    if paths["src"] != '' and paths["dest"] != '':
+    if paths["src"] != '' and paths["dest"] != '' and paths["src"] != paths["dest"]:
         return True
     else:
+        if paths["src"] == paths["dest"]:
+            tkMessageBox.showerror( "Error", "Source and destination can't be the same folder." )
         return False
-
 
 def setFolder(x,y,paths,source,bCopy):
     x['title'] = 'Select the SOURCE directory' if source else 'Select the DESTINATION directory'
-
-    print("setFolder")
     path = tkFileDialog.askdirectory(**x)
-    print(path)
     if source:
         paths["src"] = path
     else: 
@@ -84,20 +84,22 @@ def setFolder(x,y,paths,source,bCopy):
 
     y.config(text = path)
 
-    if okToCopy:
+    if okToCopy(paths):
         bCopy['state'] = 'normal'
+    else:
+        bCopy['state'] = 'disabled'
+
+def showResults(results):
+    tkMessageBox.showinfo( "Summary", "{} files moved and {} files skipped.\nSee console for details.".format( len(results["moved"]), len(results["skipped"]) ) )
 
 def edited(f,cutoff):
 	#was the file edited in the prior 24 hours
 	return True if ((cutoff - os.path.getmtime(f))/3600 <= 24) else False
 
-def moveFiles(paths):
+def moveFiles(paths, results):
     #copy ALL .txt files MODIFIED/CREATED in the past 24 hours from Folder "src" to Folder "dest"
     file_filter = "*.txt"
     cutoff = time.time()
-    results = { 'moved'  : [],
-                'skipped': []
-              }
 
     try:
         for file_ in glob.glob(paths["src"]+"/"+file_filter):
@@ -105,7 +107,7 @@ def moveFiles(paths):
             if edited(file_,cutoff):
                 editTime = datetime.datetime.fromtimestamp( int(os.path.getmtime(file_)) ).strftime("%H:%M:%S")
                 shutil.move( file_, paths["dest"] )
-                print( '{} modified at {}. Moving to batch folder "{}"'.format(file_, editTime, paths["dest"]) )
+                print( '{} modified at {}.\n-->Moving to batch folder "{}"'.format(file_, editTime, paths["dest"]) )
                 results['moved'].append( file_ )
             else:
                 print( "{} not new/modified...skipping".format(file_) )
@@ -119,9 +121,8 @@ def moveFiles(paths):
         print("{} moving {}".format(err,file_))
         pass
 
-    return results
+    showResults(results)
 
 win = setup()
-
 win.mainloop()
     
---
+++ item59_2oo.py
@@ -0,0 +1,133 @@
+import Tkinter as tk, Tkconstants, tkFileDialog, tkMessageBox
+import shutil
+import os
+import glob
+import datetime
+import time
+
+class Hq:
+    #def setup():
+    # the first code which is executed, when a new instance of a class is created. 
+    def __init__(self, win  ):
+    
+        #win = tk.Tk()        
+        win.title("Send Files to HQ (v2)")
+        self.srcPath  = ""
+        self.destPath = ""
+        self.paths    = {"src"   : "",
+                    "dest"  : ""}
+        self.text = [
+            "Send Files to HQ", 
+            "This application will move files modified or edited in the past 24 hours",
+            "from the Source folder to the Destination folder selected using the buttons below.",
+            "Select Source and Destination folders then click 'Move Staged Files'",
+        ]
+        # defining options for opening a directory
+        self.dir_opt = options = {}
+        options['initialdir'] = 'C:/'
+        options['mustexist'] = False
+        options['parent'] = win
+        options['title'] = ''
+
+        self.results = { 'moved'  : [],
+                    'skipped': []
+                  }
+
+        tk.Label(win, text = self.text[0], font = ('Arial', 18, 'bold'), pady=20).pack()
+        tk.Frame(win, height = 10).pack()
+        tk.Label(win, text = self.text[1]).pack()
+        tk.Label(win, text = self.text[2], padx = 20).pack()
+        tk.Label(win, text = self.text[3]).pack()
+
+        tk.Frame(win, height = 15).pack()
+
+        self.con1 = tk.Frame(win, height=100, width = 200, padx=10, pady=10, bd=2, relief='groove' )
+        self.con1.pack()
+
+        # options for src/dest buttons
+        self.button_opt = { 'fill': Tkconstants.BOTH, 'padx': 80, 'pady': 10}
+
+        # define buttons
+        self.bSource = tk.Button(self.con1, width=30, text='Source Folder', command= lambda: self.setFolder(True)).pack(**self.button_opt) 
+        self.labelSrc = tk.Label(self.con1, text = options['initialdir'])
+        self.labelSrc.pack()
+
+        tk.Frame(win, height = 20).pack()
+    
+        self.con2 = tk.Frame(win, height=100, width = 200,  padx=10, pady=10,  bd=2, relief='groove' )
+        self.con2.pack()
+
+        self.bDest   = tk.Button(self.con2, width=30, text='Destination Folder', command= lambda: self.setFolder(False)).pack(**self.button_opt)
+        self.labelDest   = tk.Label(self.con2, text = options['initialdir']) 
+        self.labelDest.pack()
+
+        tk.Frame(win, height = 30).pack()
+
+        self.bCopy   = tk.Button(win, state='disabled', text='Move Staged Files', pady = 10, command= lambda: self.moveFiles())
+        self.bCopy.pack() #**self.button_opt)
+
+        tk.Frame(win, height = 50).pack()
+
+        #return win
+
+    def okToCopy(self):
+        if self.paths["src"] != '' and self.paths["dest"] != '' and self.paths["src"] != self.paths["dest"]:
+            return True
+        else:
+            if self.paths["src"] == self.paths["dest"]:
+                tkMessageBox.showerror( "Error", "Source and destination can't be the same folder." )
+            return False
+
+    def setFolder(self,source):
+        win.dir_opt['title'] = 'Select the SOURCE directory' if source else 'Select the DESTINATION directory'
+        path = tkFileDialog.askdirectory(**win.dir_opt)
+        if source:
+            self.paths["src"] = path
+            self.labelSrc.config(text = path)
+        else: 
+            self.paths["dest"] = path
+            self.labelDest.config(text = path)
+
+        if self.okToCopy():
+            self.bCopy['state'] = 'normal'
+        else:
+            self.bCopy['state'] = 'disabled'
+
+    def showResults(self):
+        tkMessageBox.showinfo( "Summary", "{} files moved and {} files skipped.\nSee console for details.".format( len(self.results["moved"]), len(self.results["skipped"]) ) )
+
+    def edited(self,f,cutoff):
+	    #was the file edited in the prior 24 hours
+        return True if ((cutoff - os.path.getmtime(f))/3600 <= 24) else False
+
+    def moveFiles(self):
+        #copy ALL .txt files MODIFIED/CREATED in the past 24 hours from Folder "src" to Folder "dest"
+        file_filter = "*.txt"
+        cutoff = time.time()
+
+        try:
+            for file_ in glob.glob(self.paths["src"]+"/"+file_filter):
+
+                if self.edited(file_,cutoff):
+                    editTime = datetime.datetime.fromtimestamp( int(os.path.getmtime(file_)) ).strftime("%H:%M:%S")
+                    shutil.move( file_, self.paths["dest"] )
+                    print( '{} modified at {}.\n-->Moving to batch folder "{}"'.format(file_, editTime, self.paths["dest"]) )
+                    self.results['moved'].append( file_ )
+                else:
+                    print( "{} not new/modified...skipping".format(file_) )
+                    self.results['skipped'].append( file_ )
+
+        except IOError as err:
+            print("I/O Error ({}) moving {}.".format(err,file_))
+            pass
+
+        except Exception as err:
+            print("{} moving {}".format(err,file_))
+            pass
+
+        self.showResults()
+
+root = tk.Tk()
+win = Hq(root)
+root.mainloop()
+    
